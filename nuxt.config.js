935c755916eebc1ce91d3d1f195c010c4ccbc62f
lihaiyuan <18360710743@qq.com>
2020-10-28 18:26:44 +0800

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,43 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: '万物互联学院 - Java视频|HTML5视频|前端视频|Python视频|大数据视频-自学拿1万+月薪的IT在线视频课程，万物互联粉丝力挺，老学员为你推荐',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '万物互联学院,IT在线视频教程,Java视频,HTML5视频,前端视频,Python视频,大数据视频' },
+      { hid: 'description', name: 'description', content: '万物互联学院是国内领先的IT在线视频学习平台、职业教育平台。截止目前,万物互联学院线上、线下学习人次数以万计！会同上百个知名开发团队联合制定的Java、HTML5前端、大数据、Python等视频课程，被广大学习者及IT工程师誉为：业界最适合自学、代码量最大、案例最多、实战性最强、技术最前沿的IT系列视频课程！' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    'swiper/dist/css/swiper.css'
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    { src: '~/plugins/element-ui' },
+    { src: '~/plugins/nuxt-swiper-plugin.js', ssr: false }
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    transpile: [/^element-ui/],
+  }
+}
